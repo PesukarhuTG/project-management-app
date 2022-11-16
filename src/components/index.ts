369002a4e62fe710ea78353c88143a4bb2e3f0e9
Сергей Ляхов <serjml@Mac-Serjml.local>
2022-11-16 16:39:14 +0300

    feat: add task modal

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
 export { default as Button } from './Button';
+export { default as BoardModal } from './BoardModal';
+export { default as ColumnModal } from './ColumnModal';
+export { default as ConfirmModal } from './ConfirmModal';
+export { default as TaskModal } from './TaskModal';
