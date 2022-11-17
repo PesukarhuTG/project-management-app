1dd3133d2d833f0077c029aa32260ed315ea7a46
Сергей Ляхов <serjml@Mac-Serjml.local>
2022-11-17 14:03:26 +0300

    feat: add not found page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import { BasePage } from '../components';
+import { Result } from 'antd';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const NotFoundPage: React.FC = () => {
-  return <BasePage>404</BasePage>;
+  return (
+    <BasePage>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Something went wrong..."
+        extra={<StyledLink to="/">Back to main page</StyledLink>}
+      />
+    </BasePage>
+  );
 };
+
+const StyledLink = styled(Link)`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 33px;
+  color: var(--primary-dark);
+`;
 
 export default NotFoundPage;
