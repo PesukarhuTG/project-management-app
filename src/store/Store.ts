01a3f6b7cf5d74ee1746b01550baa4e96196a093
Сергей Ляхов <serjml@Mac-Serjml.local>
2022-11-22 16:48:47 +0300

    feat: add boards slice

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './UserSlice';
+import boardsReducer from './BoardsSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    boards: boardsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
