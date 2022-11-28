ac9a140d8b7d28aa16d34852964acb0b1977431a
Сергей Ляхов <serjml@Mac-Serjml.local>
2022-11-28 21:41:30 +0300

    feat: add search boards

--- src/components/BoardsList.tsx
+++ src/components/BoardsList.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { RootState } from '../store/Store';
 import { BoardProps } from '../types/SingleBoardProps';
 import EmptyBoard from './EmptyBoard';
 import SingleBoard from './SingleBoard';
@@ -11,12 +13,16 @@ interface BoardsListProps {
 }
 
 const BoardsList: FC<BoardsListProps> = ({ boards, remove, edit }) => {
+  const { search } = useSelector((state: RootState) => state.boards);
+
+  if (!boards.length) return <NothingFound>Nothing found</NothingFound>;
+
   return (
     <BoardsContainer>
       {boards.map((board) => (
         <SingleBoard {...board} remove={remove} edit={edit} key={board.id} />
       ))}
-      <EmptyBoard />
+      {!search && <EmptyBoard />}
     </BoardsContainer>
   );
 };
@@ -39,6 +45,19 @@ const BoardsContainer = styled.div`
   @media (max-width: 750px) {
     max-width: 426px;
     grid-template-columns: 1fr;
+  }
+`;
+
+const NothingFound = styled.h2`
+  margin-top: 40px;
+  text-align: center;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 54px;
+  color: var(--primary-dark);
+
+  @media (max-width: 1100px) {
+    margin-top: 20px;
   }
 `;
 
