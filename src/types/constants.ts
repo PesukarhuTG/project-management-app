c299b20ec34e33d0dd239ad8c23f11fb2ba0fc6a
Tatiana <ganzhinovat@yandex.ru>
2023-01-04 12:44:17 +0300

    refactor: change link to backend deploy

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,1 +1,2 @@
-export const BACKEND_URL = 'https://final-task-backend-production-bfca.up.railway.app';
+//export const BACKEND_URL = 'https://final-task-backend-production-bfca.up.railway.app';
+export const BACKEND_URL = 'https://parsimonious-drain-production.up.railway.app';
